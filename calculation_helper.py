7b71ba8ea83a2a0bb85542b7cdf21ac71b05b9ac
uraniumman2 <l.shagi@kazdream.kz>
2022-04-20 12:00:33 +0600

    [ADD] todos

--- calculation_helper.py
+++ calculation_helper.py
@@ -13,12 +13,13 @@ class CalculationHelper:
         for mms in match_member_stats:
             kd = mms['k'] / mms['d'] if mms['d'] != 0 else 0
             adr = mms['adr']
+            # TODO: ignore FD for supp
             fk = mms['fk'] * (1 - 1 / mms['k']) if mms['k'] != 0 else 0
             fd = mms['fd'] * (1 - 1 / mms['d']) if mms['d'] != 0 else 0
             v5, v4, v3, v2, v1 = mms['1v5'], mms['1v4'], mms['1v3'], mms['1v2'], mms['1v1']
             m5, m4, m3, m2, m1 = mms['m5'], mms['m4'], mms['m3'], mms['m2'], mms['m1']
             clutch_rate = v5 * 0.1 + v4 * 0.08 + v3 * 0.06 + v2 * 0.04 + v1 * 0.02
-            # TODO: if sup clutch_rate * 1.2
+            # TODO: if supp then clutch_rate * 1.2
             multi_kill = m5 * 0.05 + m4 * 0.04 + m3 * 0.03 + m2 * 0.02 + m1 * 0.01
             basic = kd * 0.2 + adr * 0.2 + fk + fd + clutch_rate + multi_kill
 
