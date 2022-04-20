09b98874f109e2214641594de1a18e2b81b13c06
LichMFK <57214899+LichMFK@users.noreply.github.com>
2022-04-20 19:01:12 +0600

    Update calculation_helper.py
    [FIX] check roles

--- helper/calculation_helper.py
+++ helper/calculation_helper.py
@@ -24,9 +24,10 @@ class CalculationHelper:
     @staticmethod
     def calculate_basic_score(mms, extra_info):
         user_id = mms['user_id']
-        role = extra_info.get(str(user_id))
-        if role is None:
+        extra_info_ = extra_info.get(str(user_id))
+        if extra_info_ is None:
             return 0
+        role = extra_info_['role']
         kd = mms['k'] / mms['d'] if mms['d'] != 0 else 0
         adr = mms['adr'] / 100
 
@@ -35,13 +36,13 @@ class CalculationHelper:
 
         v5, v4, v3, v2, v1 = mms['1v5'], mms['1v4'], mms['1v3'], mms['1v2'], mms['1v1']
         m5, m4, m3, m2, m1 = mms['m5'], mms['m4'], mms['m3'], mms['m2'], mms['m1']
-
+        
         clutch_rate = v5 * 0.1 + v4 * 0.08 + v3 * 0.06 + v2 * 0.04 + v1 * 0.02
-
         if role == Role.SUPPORT:
             clutch_rate *= 1.21
             fd = 0
-
+        if str(user_id) == "1037733":
+            print(f"uid={user_id} fk={fk} fd={fd} cr={clutch_rate} role={role}")
         multi_kill = m5 * 0.05 + m4 * 0.04 + m3 * 0.03 + m2 * 0.02 + m1 * 0.01
 
         basic = kd * 0.2 + adr * 0.2 + fk - fd + clutch_rate + multi_kill
@@ -50,10 +51,10 @@ class CalculationHelper:
     @staticmethod
     def calculate_extra_score(mms, weapon_stats, extra_info):
         user_id = mms['user_id']
-        extra_info = extra_info.get(str(user_id))
+        extra_info_ = extra_info.get(str(user_id))
         if extra_info is None:
             return 0
-        role = extra_info['role']
+        role = extra_info_['role']
         kills = mms['k']
         if role == Role.AWP:
             awp = CalculationHelper.get_weapon_kills_(user_id=user_id,
@@ -68,7 +69,7 @@ class CalculationHelper:
         else:
             extra = 0.2
 
-        grenade = extra_info['grenade']
+        grenade = extra_info_['grenade']
         if role == Role.SUPPORT:
             if grenade:
                 extra += 0.2
@@ -78,7 +79,7 @@ class CalculationHelper:
             if grenade:
                 extra += 0.1
 
-        if extra_info['capitan']:
+        if extra_info_['capitan']:
             extra += 0.1
 
         return extra
