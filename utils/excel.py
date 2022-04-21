670f22b2df13b03d0512f1d43f1437d4e1450fee
uraniumman2 <l.shagi@kazdream.kz>
2022-04-21 18:11:00 +0600

    [FIX] excel export

--- utils/excel.py
+++ utils/excel.py
@@ -15,12 +15,14 @@ def export_to_excel(match_, data):
     style_border.borders = borders
 
     work_book = xlwt.Workbook()
-    for item in data:
-        work_sheet = work_book.add_sheet(item['map'])
-        work_sheet.write(0, 0, 'Nickname', style=style_bold)
-        work_sheet.write(0, 1, 'Basic', style=style_bold)
-        work_sheet.write(0, 2, 'Extra', style=style_bold)
-        work_sheet.write(0, 3, 'Total', style=style_bold)
+    work_sheet = work_book.add_sheet(match_['id'])
+    for i, item in enumerate(data):
+        offset = i * 5
+        work_sheet.write(offset, 0, item['map'], style=style_bold)
+        work_sheet.write(offset, 1, 'Nickname', style=style_bold)
+        work_sheet.write(offset, 2, 'Basic', style=style_bold)
+        work_sheet.write(offset, 3, 'Extra', style=style_bold)
+        work_sheet.write(offset, 4, 'Total', style=style_bold)
 
         work_sheet.col(1).width = len('Basic') * 512 + 20
         work_sheet.col(2).width = len('Extra') * 512 + 20
@@ -30,9 +32,9 @@ def export_to_excel(match_, data):
             score = item['scores'][member['user_id']]
             if len(member['nickname']) > max_width:
                 max_width = len(member['nickname'])
-            work_sheet.write(index + 1, 0, member['nickname'], style=style_border)
-            work_sheet.write(index + 1, 1, round(score['basic'], 3), style=style_border)
-            work_sheet.write(index + 1, 2, round(score['extra'], 3), style=style_border)
-            work_sheet.write(index + 1, 3, round(score['total'], 3), style=style_border)
+            work_sheet.write(offset + index + 1, 1, member['nickname'], style=style_border)
+            work_sheet.write(offset + index + 1, 2, round(score['basic'], 3), style=style_border)
+            work_sheet.write(offset + index + 1, 3, round(score['extra'], 3), style=style_border)
+            work_sheet.write(offset + index + 1, 4, round(score['total'], 3), style=style_border)
         work_sheet.col(0).width = max_width * 512
         work_book.save(f"match_{match_['id']}_{match_['started_at']}.xls")
